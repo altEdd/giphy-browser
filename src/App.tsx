6186af04d6523316a2dee5e0aab2200b49baf5f2
Eddie Hedges <eddiehedges+altEdd@gmail.com>
2021-01-24 23:35:13 -0800

    Implement the initial trending screen "infinite scroll"

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import SearchBar from "./SearchBar";
+import Giphys from "./Giphys";
 
 function App() {
 	const [searchQuery, setSearchQuery] = useState("");
@@ -8,7 +9,7 @@ function App() {
 	return (
 		<div className="app">
 			<SearchBar onSearch={(sq) => setSearchQuery(sq)} />
-			{searchQuery}
+			<Giphys searchQuery={searchQuery} />
 		</div>
 	);
 }
